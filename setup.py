38b47a4b3baa341eec4edf5e28c2e7e3dc4d72c7
Jason Lerch <jason@phenogenomics.ca>
2015-08-07 08:26:37 -0400

    * added make_laplace_grid to generate a grid given a label volume and label definitions * updated contact info in setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     version = "1.0",
     description = "MINC version of Laplace's equation.",
     author = "Jason Lerch",
-    author_email = 'jason@phenogenomics.ca',
+    author_email = 'jason.lerch@utoronto.ca',
     url = 'https://github.com/jasonlerch/minclaplace',
-    scripts = ["minclaplace"],
+    scripts = ["minclaplace", "make_laplace_grid"],
     ext_modules = [Extension("cython_laplace", ["cython_laplace.c"])]
 )
